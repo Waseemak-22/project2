5c5fa69e12ff9fd07a42cbd7bf67d302d39fd72c
Waseemak-22 <waseemak673@gmail.com>
2026-05-14 22:47:37 +0530

    Add function to sum two numbers

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 // adding new feature - button
 // adding new feature - form
+// Simple JavaScript example: Add two numbers
+
+function add(a, b) {
+  return a + b;
+}
+
+const result = add(5, 3);
+
+console.log("Result:", result);
 
